2d080de7beaa7e24322ba3938e8bd6e0c7557d12
soundtemple <twwalter@gmail.com>
2018-01-26 17:53:53 +1100

    commence static pages app

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home_page, name='home_page'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^faqs$', views.faqs, name='faqs'),
+    url(r'^training', views.training, name='training'),
+]
---
+++ pages/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home_page(request):
+    return render(request, 'pages/home_page.html', {})
+
+
+def contact(request):
+    return render(request, 'pages/contact.html', {})
+
+
+def about(request):
+    return render(request, 'pages/about.html', {})
+
+
+def faqs(request):
+    return render(request, 'pages/faqs.html', {})
+
+
+def training(request):
+    return render(request, 'pages/training.html', {})
+
+
