c00113c2292d13089b17f5e3302aa9c40d499a9e
soundtemple <twwalter@gmail.com>
2018-01-31 16:43:42 +1100

    Basic CRUD actions for Blog app

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -21,6 +22,9 @@ class Post(models.Model):
 
     def __str__(self):
         return 'Post: %s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('article-detail', kwargs={'pk': self.pk})
 
 
 class Comment(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Comment, Post, Tag
+from django.utils import timezone
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 
-def test(request):
-    return render(request, 'blog/test.html', {})
+class ArticleCreateView(CreateView):
+
+    model = Post
+    fields = ['title', 'body', 'author', 'tags']
+    template_name = 'blog/article_create.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body', 'author', 'tags']
+    template_name = 'blog/article_edit.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/article_delete.html'
+    success_url = reverse_lazy('article-list')
+
+
+class ArticleListView(ListView):
+
+    model = Post
+    template_name = 'blog/article_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+
+class ArticleDetailView(DetailView):
+
+    model = Post
+    template_name = 'blog/article_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+
