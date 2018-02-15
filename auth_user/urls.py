05249ed36f1de698e92f4fede0b232629f0c6bfe
soundtemple <twwalter@gmail.com>
2018-02-16 10:35:40 +1100

    Implement user account creation and email activation

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import  url, include
+from django.conf.urls import url, include
+from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -15,4 +16,10 @@ urlpatterns += [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    url(r'^account_activation_success/$', views.account_activation_success, name='account_activation_success'),
 ]
+
