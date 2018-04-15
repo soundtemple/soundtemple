802ef9812868de97547c85bf874c5cdbe88bbb38
soundtemple <twwalter@gmail.com>
2018-04-15 18:17:11 +1000

    Comments footer widgets featured

---
+++ comments/templatetags/footer_tags.py
@@ -0,0 +1,19 @@
+from django import template
+from django.apps import apps
+
+register = template.Library()
+
+
+@register.inclusion_tag('comments/widget_testimonials.html')
+def footer_testimonials():
+    Comment = apps.get_model('comments', 'Comment')
+    featured_testimonials = Comment.objects.filter(featured=True)[:5]
+
+    return {'featured_testimonials': featured_testimonials}
+
+
+@register.inclusion_tag('comments/widget_news.html')
+def footer_news():
+    Post = apps.get_model('blog', 'Post')
+    featured_posts = Post.objects.filter(featured=True)[:4]
+    return {'featured_posts': featured_posts}
