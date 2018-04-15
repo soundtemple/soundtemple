11407578e9b988d1faf82040c066c54268e5c29d
soundtemple <twwalter@gmail.com>
2018-04-15 18:15:50 +1000

    Implement comments app initial

--- comments/views.py
+++ comments/views.py
@@ -1,13 +1,13 @@
 from django.apps import apps
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render, reverse
-from django.utils.decorators import method_decorator
-from django.views.generic.edit import BaseUpdateView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.views.generic.list import ListView
 from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.list import ListView
 
 from .forms import CommentForm
 from .models import Comment
@@ -28,39 +28,58 @@ class CommentMixin(object):
 
 
 @method_decorator(login_required, name='dispatch')
-class AddPostComment(CreateView):
+class AddComment(CreateView):
 
     model = Comment
     fields = ['text']
     template_name = 'comments/comment_add.html'
 
-    def get(self, request, pk, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         form = CommentForm(initial={'user': request.user})
-        Post = apps.get_model('blog', 'Post')
-        post = get_object_or_404(Post, pk=pk)
-
         context = {}
         context.update({
             'form': form,
-            'post': post,
-            'comment_type': Comment.COMMENT_TYPES[0][1],
         })
+
+        if 'post_id' in request.GET:
+            Post = apps.get_model('blog', 'Post')
+            post = get_object_or_404(Post, pk=request.GET['post_id'])
+            context.update({
+                'post': post,
+                'comment_type': Comment.COMMENT_TYPES[0][1],
+            })
+        if 'type' in request.GET and request.GET['type'] == 'testimonial':
+            context.update({
+                'comment_type': Comment.COMMENT_TYPES[3][1],
+            })
+        else:
+            raise Http404
 
         return render(request, self.template_name, context)
 
-    def post(self, request, pk):
-        Post = apps.get_model('blog', 'Post')
-        post = get_object_or_404(Post, pk=pk)
+    def post(self, request):
 
         form = CommentForm(request.POST)
         form.instance.user = self.request.user
 
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
-            comment.category = Comment.COMMENT_TYPES[0][0]
-            comment.save()
-            return redirect('article-detail', pk=pk)
+            if 'post_id' in request.GET:
+                # Handle news article post comment
+                Post = apps.get_model('blog', 'Post')
+                post = get_object_or_404(Post, pk=request.GET['post_id'])
+                comment.post = post
+                comment.category = Comment.COMMENT_TYPES[0][0]
+                comment.save()
+                return redirect('article-detail', pk=request.GET['post_id'])
+
+            if 'type' in request.GET and request.GET['type'] == 'testimonial':
+                # Handle testimonial submission
+                comment.category = Comment.COMMENT_TYPES[3][0]
+                comment.save()
+                return redirect(request.GET['next'])
+            else:
+                return Http404
 
 
 @method_decorator(decorators, name='dispatch')
@@ -109,3 +128,32 @@ class CommentsListView(ListView):
         return context
 
 
+class TestimonialList(ListView):
+    model = Comment
+    template_name = 'comments/testimonial_list.html'
+    ordering = ['-featured',  '-created_on']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        context['testimonials'] = Comment.objects.filter(category=Comment.COMMENT_TYPES[3][0])\
+                                .exclude(approved_comment=False).order_by('-featured', '-created_on')
+        return context
+
+
+@staff_member_required
+def comment_feature_toggle(request, pk, feature):
+    comment = get_object_or_404(Comment, pk=pk)
+    if feature == 'True':
+        comment.featured = True
+        comment.feature()
+    elif feature == 'False':
+        comment.featured = False
+        comment.unfeature()
+    else:
+        pass
+
+    if 'path' in request.GET and request.GET['path'] == 'list':
+        return HttpResponseRedirect(reverse('comment-list'))
+    else:
+        return HttpResponseRedirect(reverse('home_page'))
