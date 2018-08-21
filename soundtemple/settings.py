33b2ee687334b3013100b6ed1b8c97bd942fa234
soundtemple <twwalter@gmail.com>
2018-08-21 13:22:36 +1000

    setup AWS S3 for static files

--- soundtemple/settings.py
+++ soundtemple/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'tinymce',
     'django_extensions',
+    'storages',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
@@ -115,9 +116,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# local static url =>    STATIC_URL = '/static/'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 
 SHELL_PLUS = "ipython"
+
+# AWS SETTINGS
+AWS_STORAGE_BUCKET_NAME = 'soundtemple-static'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'soundtemple/static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
